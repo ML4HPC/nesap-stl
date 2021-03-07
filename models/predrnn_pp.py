00d0ee0ee437db9bb76691fb9fa144efe75cdf42
bj1123 <bj1123@daum.net>
2021-03-08 01:19:05 +0900

    implementation: bptt classification head

--- models/predrnn_pp.py
+++ models/predrnn_pp.py
@@ -26,10 +26,13 @@ class PredRNNPP(torch.nn.Module):
         else:
             raise ValueError(f'num_dims value {num_dims} not allowed')
 
-    def forward(self, x):
-
+    def forward(self, x, hidden_states=None):
         # Initialize hidden states
-        h, c, m, z = [None]*4
+        if hidden_states is None:
+            h, c, m, z = [None]*4
+        else:
+            assert len(hidden_states) == 4
+            h, c, m, z = hidden_states
         outputs = []
 
         # Loop over the sequence
@@ -37,8 +40,25 @@ class PredRNNPP(torch.nn.Module):
             h, c, m, z = self.clstm(x[:,t], h, c, m, z)
             outputs.append(self.decoder(h[-1])) #.permute(0, -1, 1, 2)
 
-        # Stack outputs along time axis
-        return torch.stack(outputs, dim=1)
+        # Stack outputs along time axis, last hidden states
+        return torch.stack(outputs, dim=1), (h, c, m, z)
+
+
+class RNNClassification(torch.nn.Module):
+    """
+    for classification
+    """
+    def __init__(self, filter_size, num_dims,
+                 num_hidden, num_classes):
+        super().__init__()
+        self.encoder = PredRNNPP(filter_size, num_dims, num_hidden)
+        self.classification_head = toch.nn.Linear(128, num_classes)  # 128 is hard-coded.
+        # Must be modified to be alligned with hidden state size of PredRNNPP
+
+    def foward(self, x):
+        stacks, hiddens = self.encoder(x)
+        return self.classification_head(hiddens[0])
+
 
 def build_model(**kwargs):
     return PredRNNPP(**kwargs)
--- trainers/auto_regressive.py
+++ trainers/auto_regressive.py
@@ -15,6 +15,8 @@ class AutoRegressiveTrainer(BasicTrainer):
 
     def __init__(self, **kwargs):
         super(AutoRegressiveTrainer, self).__init__(**kwargs)
+        # this is hard coded, should be modified for backpropagation through time (bptt) step to be adjustable with arguments
+        self.bptt_step = 2
 
     def train_epoch(self, data_loader):
         """Train for one epoch"""
@@ -29,19 +31,25 @@ class AutoRegressiveTrainer(BasicTrainer):
         for i, batch in enumerate(data_loader):
             batch = batch.to(self.device)
             self.model.zero_grad()
-            batch_input, batch_target = batch[:,:-1], batch[:,1:]
-            batch_output = self.model(batch_input)
-            batch_loss = self.loss_func(batch_output, batch_target)
-            batch_loss.backward()
+            hidden_states = None
+            for j in range(self.bptt_step):  # chunk batch for bptt.
+                num_step = batch.size(1)
+                cur_batch = batch[:,num_step*j:num_step*(j+1)]
+                batch_input, batch_target = cur_batch[:,:-1], cur_batch[:,1:]
+                batch_output, hidden_states = self.model(batch_input, hidden_states)
+                batch_loss = self.loss_func(batch_output, batch_target)
+                batch_loss.backward()
+                hidden_states = (item.detach() for item in hidden_states)
+                sum_loss += batch_loss.item()
+                utils.metrics.update_metrics(self.metrics, batch_output, batch_target)
+                self.logger.debug('batch %i loss %.3f', i, batch_loss.item())
+                self.logger.debug('cuda mem %g max %g',
+                                  torch.cuda.memory_allocated() / 1024 ** 3,
+                                  torch.cuda.max_memory_allocated() / 1024 ** 3)
             self.optimizer.step()
-            sum_loss += batch_loss.item()
-            utils.metrics.update_metrics(self.metrics, batch_output, batch_target)
-            self.logger.debug('batch %i loss %.3f', i, batch_loss.item())
-            self.logger.debug('cuda mem %g max %g',
-                              torch.cuda.memory_allocated()/1024**3,
-                              torch.cuda.max_memory_allocated()/1024**3)
 
-        train_loss = sum_loss / (i + 1)
+
+        train_loss = sum_loss / (i * self.bptt_step + 1)
         metrics_summary = utils.metrics.get_results(self.metrics)
         self.logger.debug('Processed %i batches' % (i + 1))
 
