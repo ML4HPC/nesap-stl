be53733887718297012c82edf7388854169cd949
Seungwook Han <sh3264@coumbia.edu>
2021-03-08 18:31:22 +0900

    Fixed script for gpu setting

--- scripts/train_prnn3d_fmri.sh
+++ scripts/train_prnn3d_fmri.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-#SBATCH -C haswell
-#SBATCH -q debug
-#SBATCH -t 30
-#SBATCH -J train-cori
+
+#SBATCH -C gpu
+#SBATCH --cpus-per-task=10
+#SBATCH --ntasks-per-node=8
+#SBATCH --gpus-per-task=1
+#SBATCH --exclusive
+#SBATCH --time 4:00:00
+#SBATCH -J prnn3d_fmri
 #SBATCH -o logs/%x-%j.out
 
 # Setup software
-module load cgpu pytorch/1.5.0-gpu
-export OMP_NUM_THREADS=32
-export KMP_AFFINITY="granularity=fine,compact ,1,0"
-export KMP_BLOCKTIME=1
+module load cgpu pytorch/v1.5.1-gpu
 
 # Run the training
-srun -l -u python train.py --config configs/prnn3d_fmri.yaml -d mpi --gpus 0 1 2 3 $@
+# srun -l -u python train.py -d nccl --rank-gpu $@
+
+srun -l -u python train.py --config configs/prnn3d_fmri.yaml -d nccl --gpus 0 1 2 3 $@
