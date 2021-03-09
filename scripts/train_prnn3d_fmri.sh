748cac1d84564881ca89f731bf77cee14d3c42e3
Seungwook Han <sh3264@coumbia.edu>
2021-03-09 13:38:11 +0900

    Slurm script not working

--- scripts/train_prnn3d_fmri.sh
+++ scripts/train_prnn3d_fmri.sh
@@ -5,13 +5,11 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --exclusive
 #SBATCH --time 4:00:00
-#SBATCH -J prnn3d_fmri
+#SBATCH -J train-cgpu
 #SBATCH -o logs/%x-%j.out
 
 # Setup software
-module load cgpu pytorch/v1.5.1-gpu
+module load pytorch/v1.5.1-gpu
 
 # Run the training
-# srun -l -u python train.py -d nccl --rank-gpu $@
-
 srun -l -u python train.py --config configs/prnn3d_fmri.yaml -d nccl --rank-gpu $@
